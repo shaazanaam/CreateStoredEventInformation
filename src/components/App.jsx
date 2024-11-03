2f14d2390599a2eabedf3f7cef8e15a7eef022a7
Shaaz Anaam <anaamsz@mail.uc.edu>
2024-11-03 11:09:29 -0600

    This method shows the wrong way of using the if statement which removes the other part of the object property from the hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,38 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setState]=useState("");
-  const [inputValue, setInputValue]=useState("");
-  const [lName,setLname]=useState("")
-
-  console.log (name)
-  function handleSubmit(){
-    setInputValue(" " +name +" "+ lName)
-    event.preventDefault();
-  }
-  function handleChange(event) {
-    setState(event.target.value);
-   
-    
-  }
-  function handleChangeLName(event) {
-    setLname(event.target.value);
-   
-    
-  }
+  const [fullName, setFullName]=useState({
+    fName: "",
+    lName:""
+  });
+ 
   
+
+
+ 
+function handleChange(event){
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+   
+    if (inputName ==="fName"){
+      setFullName({fName:newValue})
+    } else if (inputName ==="lName"){
+      setFullName({lName:newValue})
+    }
+
+    console.log(newValue);
+    console.log(inputName);
+}
   return (
     <div className="container">
-      <h1>Hello {name} {lName}</h1>
-      <form onSubmit={handleSubmit}>
-        <input  onChange={handleChange} name="fName" placeholder="First Name" value={name} />
-        <input onChange={handleChangeLName} name="lName" placeholder="Last Name"  value={lName} />
+      <h1>Hello {fullName.fName} {fullName.lName} </h1>
+      <form >
+        <input  onChange={handleChange} name="fName" placeholder="First Name" 
+         //value={fullName.fName}
+          />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" 
+         //value ={fullName.lName} 
+         />
         <button type ="submit">Submit</button>
       </form>
     </div>
