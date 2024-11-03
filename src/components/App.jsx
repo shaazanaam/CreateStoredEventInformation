81b0b41015538f9c0022d988d9302f6fa98d7d2d
Shaaz Anaam <anaamsz@mail.uc.edu>
2024-11-03 11:46:19 -0600

    This is the Verbose version of showing the exmaple using the previous state value directly from the react rendering of the statefull control elements since the begining

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function App() {
   const [fullName, setFullName]=useState({
     fName: "",
-    lName:""
+    lName:"",
   });
  
   
@@ -14,24 +14,31 @@ function handleChange(event){
     const newValue = event.target.value;
     const inputName = event.target.name;
    
-    if (inputName ==="fName"){
-      setFullName({fName:newValue})
-    } else if (inputName ==="lName"){
-      setFullName({lName:newValue})
+   setFullName(preValue=> {
+    if(inputName==="fName"){
+    return{
+      fName:newValue,
+      lName: preValue.lName
     }
+   }  else if(inputName ==="lName"){
+    return{
+      fName:preValue.fName,
+      lName: newValue,
+    }
+   }
+  });
 
-    console.log(newValue);
-    console.log(inputName);
+   
 }
   return (
     <div className="container">
       <h1>Hello {fullName.fName} {fullName.lName} </h1>
       <form >
         <input  onChange={handleChange} name="fName" placeholder="First Name" 
-         //value={fullName.fName}
+         value={fullName.fName}
           />
         <input onChange={handleChange} name="lName" placeholder="Last Name" 
-         //value ={fullName.lName} 
+         value ={fullName.lName} 
          />
         <button type ="submit">Submit</button>
       </form>
