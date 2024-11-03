74fc6a9ecaa44095a570a7319656ecf44fa22fce
Shaaz Anaam <anaamsz@mail.uc.edu>
2024-11-03 10:11:42 -0600

    Logic for changing the formInput+Submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setState]=useState("");
+  const [inputValue, setInputValue]=useState("");
+  const [lName,setLname]=useState("")
+
+  console.log (name)
+  function handleSubmit(){
+    setInputValue(" " +name +" "+ lName)
+    event.preventDefault();
+  }
+  function handleChange(event) {
+    setState(event.target.value);
+   
+    
+  }
+  function handleChangeLName(event) {
+    setLname(event.target.value);
+   
+    
+  }
+  
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+      <h1>Hello {name} {lName}</h1>
+      <form onSubmit={handleSubmit}>
+        <input  onChange={handleChange} name="fName" placeholder="First Name" value={name} />
+        <input onChange={handleChangeLName} name="lName" placeholder="Last Name"  value={lName} />
+        <button type ="submit">Submit</button>
       </form>
     </div>
   );
